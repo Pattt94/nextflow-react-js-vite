4ab26647974e6964272ca2751ea3a62de64db989
Pattt94 <101586367+Pattt94@users.noreply.github.com>
2025-07-02 09:05:58 +0000

    feat: refactor App component to use Hello and Goodbye functions for better structure

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,40 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const Hello = (props) => {
+
+  if (props.username == undefined) {
+    return <p>Hello</p>
+  }
+  return (
+    <div className='paleblue'>
+      <h1>Hello!, {props.username}</h1>
+    </div>
+  );
+}
+
+function Goodbye () {
+  return (
+    <div>
+      <h2>Bye, bye</h2>
+    </div>
+
+  )
+}
+
 function App() {
   let city ="Seoul"
 
+  const onGreet = () => {
+    alert("안녕하세요")
+  };
+
   return (
     <div>
-      <h1 id="header" className='paleblue'>Hello, {city} :)</h1>
+      <Hello username={city}/>
+      <Hello username="Annie"/>
+      <Goodbye/>
+      <button onClick={onGreet}>Greeting</button>
     </div>
   );
   
